d4b23705b512522235a9f50a42a53a94b8a84baf
Beatriz Martinez Gonzalez <7704645+imagobea@users.noreply.github.com>
2022-05-25 18:57:55 +0200

    pack nightfall sdk (#1)
    * ported sdk files
    * nf3: replaced logger with console for now, deprecated `getNewBlockEmitter` and `getChallengerEmitter`
    * added package.json
    * added .gitignore

---
+++ constants.mjs
@@ -0,0 +1,29 @@
+export const TOKEN_TYPE = {
+  ERC20: 'ERC20',
+  ERC721: 'ERC721',
+  ERC1155: 'ERC1155',
+};
+
+export const TX_TYPES = {
+  DEPOSIT: 'deposit',
+  TRANSFER: 'transfer',
+  WITHDRAW: 'withdraw',
+  INSTANT_WITHDRAW: 'instant-withdraw',
+};
+
+export const APPROVE_AMOUNT =
+  '115792089237316195423570985008687907853269984665640564039457584007913129639935';
+
+export const DEFAULT_FEE = 10;
+export const DEFAULT_PROPOSER_BOND = 10;
+export const DEFAULT_BLOCK_STAKE = 1;
+
+export const WEBSOCKET_PING_TIME = 15000;
+
+export const GAS_MULTIPLIER = Number(process.env.GAS_MULTIPLIER) || 2;
+export const GAS_PRICE_MULTIPLIER = Number(process.env.GAS_PRICE_MULTIPLIER) || 2;
+export const GAS = process.env.GAS || 4000000;
+export const GAS_PRICE = process.env.GAS_PRICE || '10000000000';
+export const GAS_ESTIMATE_ENDPOINT =
+  process.env.GAS_ESTIMATE_ENDPOINT ||
+  'https://vqxy02tr5e.execute-api.us-east-2.amazonaws.com/production/estimateGas';
