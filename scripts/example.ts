953b3f25318f22b348f8bd8750439c27fe18bb11
imagobea <himagobea@gmail.com>
2022-06-05 13:35:24 +0200

    fix: example script

--- scripts/example.ts
+++ scripts/example.ts
@@ -1,5 +1,4 @@
-// npx ts-node src/foo.ts
-// tsc && node scripts/example.ts
+// npx ts-node scripts/example.ts
 
 import User from "../src/user-client/user";
 
@@ -7,14 +6,17 @@ const ENVIRONMENT = "development";
 // const localPrivateKey =
 //   "0x4775af73d6dc84a0ae76f8726bda4b9ecf187c377229cb39e1afa7a18236a69e";
 
-// const checkStatus = async () => {
-//   await user.checkStatus();
-// };
-// console.log(await checkStatus());
+const main = async () => {
+  try {
+    const user = new User(ENVIRONMENT);
+    const status = await user.checkStatus();
+    console.log(status);
+    console.log("Bye bye");
 
-const user = new User(ENVIRONMENT);
-const status = await user.checkStatus();
-console.log(status);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
 
-console.log("Bye bye");
-process.exit();
+main();
