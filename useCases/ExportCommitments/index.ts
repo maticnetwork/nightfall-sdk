147227174dae084f03da1423ef1a18435ab3df9c
Luiz <luiz.oamorim@gmail.com>
2022-06-15 06:30:05 -0300

    test: unit tests for convert object to string and export file functions

---
+++ useCases/ExportCommitments/index.ts
@@ -0,0 +1,13 @@
+import fs from "fs";
+
+const exportCommitments = async (pathFileName: string, content: string) => {
+  return fs.writeFileSync(pathFileName, content);
+};
+
+const convertObjectToString = (obj: object) => {
+  if (typeof obj !== "object") {
+    throw new Error("The parameter passed is not an object!");
+  }
+  return JSON.stringify(obj);
+};
+export { exportCommitments, convertObjectToString };
