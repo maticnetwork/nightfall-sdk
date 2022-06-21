9964e174534a97f9ff4e2cf2f28d28dd5ec801ef
imagobea <himagobea@gmail.com>
2022-06-21 14:58:23 +0200

    refactor: add a message to client info logs

--- libs/client/client.ts
+++ libs/client/client.ts
@@ -21,7 +21,10 @@ class Client {
     try {
       res = await axios.get(`${this.apiUrl}/healthcheck`);
       if (res.status !== 200) throw new Error("Client unavailable");
-      logger.info({ status: res.status, data: res.data });
+      logger.info(
+        { status: res.status, data: res.data },
+        "Client at healthcheck responded",
+      );
     } catch (err) {
       logger.error(err);
       return false;
@@ -34,7 +37,10 @@ class Client {
     let res: AxiosResponse;
     try {
       res = await axios.get(`${this.apiUrl}/contract-address/${contractName}`);
-      logger.info({ status: res.status, data: res.data });
+      logger.info(
+        { status: res.status, data: res.data },
+        "Client at contract-address responded",
+      );
     } catch (err) {
       logger.child({ contractName }).error(err);
       return null;
@@ -47,17 +53,20 @@ class Client {
     validMnemonic: string,
     addressIndex: number,
   ) {
-    const _logInput = { validMnemonic, addressIndex }; // TODO review internal vars underscore
-    logger.debug(_logInput, "Calling client at generate-keys");
+    const logInput = { validMnemonic, addressIndex }; // TODO review internal vars underscore
+    logger.debug(logInput, "Calling client at generate-keys");
     let res: AxiosResponse;
     try {
       res = await axios.post(`${this.apiUrl}/generate-keys`, {
         mnemonic: validMnemonic,
         path: `m/44'/60'/0'/${addressIndex}`,
       });
-      logger.info({ status: res.status, data: res.data });
+      logger.info(
+        { status: res.status, data: res.data },
+        "Client at generate-keys responded",
+      );
     } catch (err) {
-      logger.child(_logInput).error(err);
+      logger.child(logInput).error(err);
       return null;
     }
     return res.data;
@@ -72,7 +81,10 @@ class Client {
         ivks: [zkpKeys.ivk],
         nsks: [zkpKeys.nsk],
       });
-      logger.info({ status: res.status, data: res.data });
+      logger.info(
+        { status: res.status, data: res.data },
+        "Client at incoming-viewing-key responded",
+      );
     } catch (err) {
       logger.child({ zkpKeys }).error(err);
       return null;
@@ -88,8 +100,8 @@ class Client {
     nsk: string,
     fee: number,
   ) {
-    const _logInput = { tokenAddress, tokenStandard, value, pkd, nsk, fee };
-    logger.debug(_logInput, "Calling client at deposit");
+    const logInput = { tokenAddress, tokenStandard, value, pkd, nsk, fee };
+    logger.debug(logInput, "Calling client at deposit");
     let res: AxiosResponse;
     try {
       res = await axios.post(`${this.apiUrl}/deposit`, {
@@ -101,9 +113,12 @@ class Client {
         nsk,
         fee,
       });
-      logger.info({ status: res.status, data: res.data });
+      logger.info(
+        { status: res.status, data: res.data },
+        "Client at deposit responded",
+      );
     } catch (err) {
-      logger.child({ _logInput }).error(err);
+      logger.child({ logInput }).error(err);
       return null;
     }
     return res.data;
