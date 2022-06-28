2dc589fc85edf7ecc82f9e00a158231730448479
Luiz <luiz.oamorim@gmail.com>
2022-06-28 15:29:28 -0300

    test: add unit test for concat two arrays functionality

---
+++ __tests__/unit/utils/concatTwoArrays.test.ts
@@ -0,0 +1,22 @@
+import { concatTwoArrays } from "../../../libs/utils/concatTwoArrays";
+
+describe("Suit of tests for concat array", () => {
+  const OBJECT1 = [
+    { table: "commitments", rows: [] },
+    { table: "transactions", rows: [] },
+  ];
+  const OBJECT2 = [
+    { table: "keys", rows: [] },
+    { table: "timber", rows: [] },
+  ];
+  test("should concat two arrays", async () => {
+    const concatedArrays = concatTwoArrays(OBJECT1, OBJECT2);
+    expect(concatedArrays).toEqual(OBJECT1.concat(OBJECT2));
+  });
+
+  test("should pass a null object and thown an error.", async () => {
+    expect(() => {
+      concatTwoArrays(null, OBJECT2);
+    }).toThrowError("Array can't be null!");
+  });
+});
