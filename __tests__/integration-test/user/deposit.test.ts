d2e6a9b9ce8f2fe58aee001db96da0589e03b009
David Rodriguez Garcia <70919589+daveroga@users.noreply.github.com>
2022-07-05 11:59:22 +0200

    Return transactionHashL2 in deposit (#50)
    * feat: return transactionHashL2 in deposit
    
    * fix: lint issues

---
+++ __tests__/integration-test/user/deposit.test.ts
@@ -0,0 +1,50 @@
+import { User } from "../../../libs/user";
+import * as dotenv from "dotenv";
+import path from "path";
+
+const _rootPath = path.resolve();
+dotenv.config({ path: path.join(_rootPath, ".env") });
+
+// Script config for goerli
+const environment = {
+  blockchainNetwork: process.env.SDK_ENV_BLOCKCHAIN_NETWORK || '',
+  blockchainWsUrl: process.env.SDK_ENV_BLOCKCHAIN_WEBSOCKET_URL || '',
+  clientApiUrl: process.env.SDK_ENV_CLIENT_API_URL || '',
+};
+const ethereumPrivateKey = process.env.SDK_ETH_PRIVATE_KEY || '';
+const tokenAddress = process.env.SDK_ETH_TOKEN_ADDRESS || ''; // MATIC contract address in goerli
+const tokenStandard = "ERC20";
+const value = "0.0014";
+
+jest.setTimeout(50000);
+
+describe("Suit for tests deposits", () => {
+  let user: User;
+
+  beforeAll(async () => {    
+    user = new User(environment); // now goerli   
+    await user.init({
+      ethereumPrivateKey,
+    });
+  });
+
+  test("should check the status for the user", async () => {         
+    const status = await user.checkStatus();
+    expect(status.isClientAlive).toBe(true);
+    expect(status.isWeb3WsAlive).toBe(true);
+  });
+  
+  test("should create a deposit", async () => {         
+      const res = await user.makeDeposit({
+        tokenAddress,
+        tokenStandard,
+        value,
+      });
+      expect(res).toHaveProperty('transactionHash');
+      expect(res).toHaveProperty('transactionHashL2');
+  });
+
+  afterAll(async () => {
+    user.close();
+  });
+});
