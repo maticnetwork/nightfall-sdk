82c7d08fb532b1f71a6fd6dce2aca6b59cba0fef
imagobea <himagobea@gmail.com>
2022-07-29 18:35:42 +0200

    feat: finaliseWithdrawal - new transaction

--- libs/transactions/index.ts
+++ libs/transactions/index.ts
@@ -1,11 +1,13 @@
 import { createAndSubmitApproval } from "./approval";
 import { createAndSubmitDeposit } from "./deposit";
 import { createAndSubmitWithdrawal } from "./withdrawal";
+import { createAndSubmitFinaliseWithdrawal } from "./withdrawalFinalise";
 import { stringValueToWei } from "./helpers/units";
 
 export {
   createAndSubmitApproval,
   createAndSubmitDeposit,
   createAndSubmitWithdrawal,
+  createAndSubmitFinaliseWithdrawal,
   stringValueToWei,
 };
