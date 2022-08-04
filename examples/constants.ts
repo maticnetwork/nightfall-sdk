6eca9455ec77ee56fddfe708bd3b1a2f42162c23
imagobea <himagobea@gmail.com>
2022-08-04 09:05:29 +0200

    refactor: env vars wip

--- examples/constants.ts
+++
@@ -1,6 +0,0 @@
-export const BLOCKCHAIN_WEBSOCKET_URL_DEFAULT = "ws://localhost:8546";
-export const CLIENT_API_URL_DEFAULT = "http://localhost:8080";
-export const ETH_PRIVATE_KEY_DEFAULT =
-  "0x4775af73d6dc84a0ae76f8726bda4b9ecf187c377229cb39e1afa7a18236a69e"; // Nightfall_3
-export const TOKEN_ADDRESS_DEFAULT =
-  "0x4f3c4F8D4575Cf73c2FAf9F36cc505e19E65B9C0"; // Nightfall_3 ERC20Mock contract address in ganache
--- examples/deposit.ts
+++ examples/deposit.ts
@@ -1,50 +1,39 @@
-import {
-  BLOCKCHAIN_WEBSOCKET_URL_DEFAULT,
-  CLIENT_API_URL_DEFAULT,
-  ETH_PRIVATE_KEY_DEFAULT,
-  TOKEN_ADDRESS_DEFAULT,
-} from "./constants";
 import { UserFactory } from "../libs/user";
 import * as dotenv from "dotenv";
 import path from "path";
 
+// Set up environment
+const environment = process.argv[2];
+
+// Based on environment, read vars from envFile
 const rootPath = path.resolve();
-dotenv.config({ path: path.join(rootPath, "examples/.env") });
+const envFile = ".env." + environment;
+dotenv.config({ path: path.join(rootPath, "examples", envFile) });
 
-/*
-  SCRIPT ARRANGEMENTS
-  -------------------
-  * Pass `dev` to UserFactory.create when connecting to ganache
-  * Pass `test` when connecting to testnet
-  * Remember `nightfallMnemonic` is optional
-*/
-
-const dev = {
-  clientApiUrl: CLIENT_API_URL_DEFAULT,
-  blockchainWsUrl: BLOCKCHAIN_WEBSOCKET_URL_DEFAULT, // ganache
-  ethereumPrivateKey: ETH_PRIVATE_KEY_DEFAULT,
+// App config
+const config = {
+  blockchainWsUrl: process.env.APP_BLOCKCHAIN_WEBSOCKET_URL,
+  clientApiUrl: process.env.APP_CLIENT_API_URL,
+  ethereumPrivateKey: process.env.APP_ETH_PRIVATE_KEY,
+  nightfallMnemonic: process.env.APP_NIGHTFALL_MNEMONIC,
+  tokenAddress: process.env.APP_TOKEN_ADDRESS_1,
 };
 
-const test = {
-  clientApiUrl: process.env.SDK_CLIENT_API_URL,
-  blockchainWsUrl: process.env.SDK_BLOCKCHAIN_WEBSOCKET_URL, // goerli
-  ethereumPrivateKey: process.env.SDK_ETH_PRIVATE_KEY,
-  nightfallMnemonic: process.env.SDK_NIGHTFALL_MNEMONIC,
-};
-
-const tokenAddress = TOKEN_ADDRESS_DEFAULT; // for goerli rely on process.env.TOKEN_ADDRESS
-
-/*
-  ACTUAL SCRIPT
-*/
-
+// Example script
 const main = async () => {
   let user;
   try {
-    user = await UserFactory.create(dev);
+    user = await UserFactory.create({
+      blockchainWsUrl: config.blockchainWsUrl,
+      clientApiUrl: config.clientApiUrl,
+      ethereumPrivateKey: config.ethereumPrivateKey,
+      nightfallMnemonic: config.nightfallMnemonic,
+    });
+
     const status = await user.checkStatus();
     console.log(status);
 
+    const tokenAddress = config.tokenAddress;
     const tokenStandard = "ERC20";
     const value = "0.0001";
     const deposit = await user.makeDeposit({
