18f9a4349d804ea6e5f289c91d3f1dcb748b99e9
Luiz <luiz.oamorim@gmail.com>
2022-08-05 10:13:28 -0300

    chore(utils): add a function that verify if all the commitments is from a specific mnemonic

---
+++ libs/utils/isCommitmentFromMnemonic.ts
@@ -0,0 +1,31 @@
+import { Commitment } from "libs/types";
+
+let allCommitmentsMatchWithCompressedZkpPK = true;
+
+/**
+ * @function isCommitmentsFromMnemonic should verify if all commitments in the list
+ * belongs to the compressed zkp public key.
+ * @param listOfCommitments a list of commitments to be verified.
+ * @param compressedZkpPublicKey the compressed key derivated from the mnemonic that shall be
+ * in the environment variables.
+ * @returns true if all commitments match or false if don't match.
+ * @author luizoamorim
+ */
+async function isCommitmentsFromMnemonic(
+  listOfCommitments: Commitment[],
+  compressedZkpPublicKey: string,
+): Promise<boolean> {
+  for (const commitment of listOfCommitments) {
+    if (commitment.compressedZkpPublicKey !== compressedZkpPublicKey) {
+      allCommitmentsMatchWithCompressedZkpPK = false;
+      break;
+    }
+  }
+
+  if (allCommitmentsMatchWithCompressedZkpPK) {
+    return true;
+  }
+  return false;
+}
+
+export default isCommitmentsFromMnemonic;
