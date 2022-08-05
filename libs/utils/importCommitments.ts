13e271826f5ba828a3246372b3d5eafcc74df4ec
Luiz <luiz.oamorim@gmail.com>
2022-08-05 10:10:06 -0300

    chore(utils): add the import commitments function

---
+++ libs/utils/importCommitments.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+import { Commitment } from "libs/types";
+import isCommitmentType from "./isCommitmentType";
+
+/**
+ *
+ * @function importFile import a file and transform the cotent in a JSON format.
+ * @param pathFileName an string with the path + file name.
+ * @returns a list of commitments.
+ * @author luizoamorim
+ */
+const importCommitments = async (
+  pathFileName: string,
+): Promise<Commitment[] | Error> => {
+  const file = fs.readFileSync(pathFileName);
+  try {
+    const commitments: Commitment[] = JSON.parse(file.toString("utf8"));
+    // Verify if file in in the correct format
+    for (const commitment of commitments) {
+      isCommitmentType(commitment);
+    }
+    return commitments;
+  } catch (err) {
+    return err;
+  }
+};
+
+export default importCommitments;
