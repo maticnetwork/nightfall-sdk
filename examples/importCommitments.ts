f844baa2f5d8d97e94f02313fc87fa9f086c1341
Luiz <luiz.oamorim@gmail.com>
2022-08-15 20:46:52 -0300

    refactor: updating the code for user creation

--- examples/importCommitments.ts
+++ examples/importCommitments.ts
@@ -1,24 +1,5 @@
-import * as dotenv from "dotenv";
-import path from "path";
-import {
-  UserFactory,
-  BLOCKCHAIN_WS_URL_DEFAULT,
-  CLIENT_API_URL_DEFAULT,
-} from "../libs/user";
-
-const _rootPath = path.resolve();
-dotenv.config({ path: path.join(_rootPath, ".env") });
-
-const ETHEREUM_PRIVATE_KEY =
-  "0x4775af73d6dc84a0ae76f8726bda4b9ecf187c377229cb39e1afa7a18236a69e";
-const NIGHTFALL_MNEMONIC = process.env.SDK_NIGHTFALL_MNEMONIC;
-
-const options = {
-  blockchainWsUrl: BLOCKCHAIN_WS_URL_DEFAULT,
-  clientApiUrl: CLIENT_API_URL_DEFAULT,
-  ethereumPrivateKey: ETHEREUM_PRIVATE_KEY,
-  nightfallMnemonic: NIGHTFALL_MNEMONIC,
-};
+import { UserFactory } from "../libs/user";
+import { config } from "./appConfig";
 
 /**
  * @function main a script to test the import commitments flow
@@ -27,7 +8,11 @@ const options = {
 const main = async () => {
   let user;
   try {
-    user = await UserFactory.create(options);
+    user = await UserFactory.create({
+      blockchainWsUrl: config.blockchainWsUrl,
+      clientApiUrl: config.clientApiUrl,
+      ethereumPrivateKey: config.ethereumPrivateKey,
+    });
 
     await user.importAndSaveCommitments(
       "./",
