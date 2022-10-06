59b80bc0f6a9cc52ea12e1f46b7aad229a0bf53f
Stefan Velkoski <110013977+velkoskis@users.noreply.github.com>
2022-10-06 16:23:06 +0200

    Velkoskis/example scripts refactor (#127)
    * refactor: refactor deposit, transfer and withdrawal
    
    * fix: connection close on scripts
    
    * refactor: change APP_TOKEN_ADDRESS_1 to APP_TOKEN_ADDRESS for clarity
    
    * style: remove unused web3 imports

--- examples/scripts/txWithdrawal.ts
+++ examples/scripts/txWithdrawal.ts
@@ -1,8 +1,19 @@
 import { UserFactory } from "../../libs/user";
 import { config } from "./appConfig";
+import type { AbiItem } from "web3-utils";
+import erc165Abi from "../../libs/tokens/abis/ERC165.json";
+import { ERC20, ERC721, ERC1155 } from "../../libs/tokens/constants";
+
+const erc165AbiItem = erc165Abi as unknown as AbiItem;
 
 const main = async () => {
   let user;
+  let tokenType;
+  let tokenId;
+  let value;
+  let web3;
+  const tokenContractAddress = config.tokenContractAddress;
+
   try {
     // # 1 Create an instance of User
     user = await UserFactory.create({
@@ -11,16 +22,50 @@ const main = async () => {
       ethereumPrivateKey: config.ethereumPrivateKey,
       nightfallMnemonic: config.nightfallMnemonic,
     });
+    web3 = user.web3Websocket.web3;
 
+    try {
+      const ercContract = new web3.eth.Contract(
+        erc165AbiItem,
+        tokenContractAddress,
+      );
+      const interface721 = await ercContract.methods
+        .supportsInterface("0x80ac58cd")
+        .call(); // ERC721 interface
+
+      if (interface721) {
+        tokenType = ERC721;
+        value = "0";
+
+        // TokenId to be withdrawn
+        tokenId =
+          "28948022309329048855892746252171976963317496166410141009864396001978282410025";
+      } else {
+        const interface1155 = await ercContract.methods
+          .supportsInterface("0xd9b67a26")
+          .call(); // ERC1155 interface
+
+        if (interface1155) {
+          tokenType = ERC1155;
+          tokenId = "4";
+          value = "2000";
+        }
+      }
+    } catch {
+      // Expected ERC20
+      tokenType = ERC20;
+      value = "0.000001";
+      tokenId = "0x00";
+    }
+
+    const tokenErcStandard = tokenType;
     // # 2 Make withdrawal
-    const tokenContractAddress = config.tokenContractAddress;
-    const tokenErcStandard = "ERC20";
-    const value = "0.0001";
     const recipientEthAddress = "0x9C8B2276D490141Ae1440Da660E470E7C0349C63";
     const txReceipts = await user.makeWithdrawal({
       tokenContractAddress,
       tokenErcStandard,
       value,
+      tokenId,
       recipientEthAddress,
       // isOffChain: true,
     });
