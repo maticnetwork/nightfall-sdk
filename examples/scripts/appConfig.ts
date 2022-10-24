20c5ed8c46660b5bf5c4854d23b9f1ba9842e604
imagobea <himagobea@gmail.com>
2022-10-24 20:33:55 +0200

    test(whichtokenstandard): integration tests for util detecting interface ids via erc165

--- examples/scripts/appConfig.ts
+++ examples/scripts/appConfig.ts
@@ -3,7 +3,7 @@ export const config = {
   nightfallMnemonic: process.env.APP_NIGHTFALL_MNEMONIC,
   ethereumPrivateKey: process.env.APP_ETH_PRIVATE_KEY,
   blockchainWsUrl: process.env.APP_BLOCKCHAIN_WEBSOCKET_URL,
-  tokenContractAddress: process.env.APP_TOKEN_ADDRESS,
+  tokenContractAddress: process.env.APP_TOKEN_ERC20,
   value: process.env.APP_TX_VALUE,
   tokenId: process.env.APP_TX_TOKEN_ID,
 };
