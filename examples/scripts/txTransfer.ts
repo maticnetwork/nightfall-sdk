e1b16c0acf07d657814e1bcf8da393e80fc6d842
Stefan Velkoski <110013977+velkoskis@users.noreply.github.com>
2022-11-08 18:08:44 +0100

    refactor: transfer and withdrawal (#145)

--- examples/scripts/txTransfer.ts
+++ examples/scripts/txTransfer.ts
@@ -1,19 +1,10 @@
 import { UserFactory } from "../../libs/user";
 import { config } from "./appConfig";
-import type { AbiItem } from "web3-utils";
-import erc165Abi from "../../libs/tokens/abis/ERC165.json";
-import { ERC20, ERC721, ERC1155 } from "../../libs/tokens/constants";
-
-const erc165AbiItem = erc165Abi as unknown as AbiItem;
 
 const main = async () => {
   let userSender;
   let userRecipient;
-  let tokenType;
-  let value;
-  let tokenId;
-  let web3;
-  const tokenContractAddress = config.tokenContractAddress;
+
   try {
     // # 1 Create an instance of User
     userSender = await UserFactory.create({
@@ -22,7 +13,6 @@ const main = async () => {
       ethereumPrivateKey: config.ethereumPrivateKey,
       nightfallMnemonic: config.nightfallMnemonic,
     });
-    web3 = userSender.web3Websocket.web3;
 
     // # 2 [OPTIONAL] For this example, we create a 2nd instance
     userRecipient = await UserFactory.create({
@@ -31,59 +21,33 @@ const main = async () => {
       ethereumPrivateKey: config.ethereumPrivateKey,
     });
 
-    try {
-      const ercContract = new web3.eth.Contract(
-        erc165AbiItem,
-        tokenContractAddress,
-      );
-      const interface721 = await ercContract.methods
-        .supportsInterface("0x80ac58cd")
-        .call(); // ERC721 interface
+    // # 3 [OPTIONAL] If you did not pass a mnemonic, you can retrieve it
+    const mnemonic = userSender.getNightfallMnemonic();
 
-      if (interface721) {
-        tokenType = ERC721;
-        value = "0";
+    // # 4 [OPTIONAL] You can check API Client, blockchain ws connection
+    const isClientAlive = await userSender.isClientAlive();
+    const isWeb3WsAlive = await userSender.isWeb3WsAlive();
+    console.log(
+      `API Client alive: ${isClientAlive}, blockchain ws alive: ${isWeb3WsAlive}`,
+    );
 
-        // TokenId to be transferred
-        tokenId =
-          "28948022309329048855892746252171976963317496166410141009864396001978282410024";
-      } else {
-        const interface1155 = await ercContract.methods
-          .supportsInterface("0xd9b67a26")
-          .call(); // ERC1155 interface
-
-        if (interface1155) {
-          tokenType = ERC1155;
-          tokenId = "4";
-          value = "2000";
-        }
-      }
-    } catch {
-      // Expected ERC20
-      tokenType = ERC20;
-      value = "0.0001";
-      tokenId = "0x00";
-    }
-
-    // # 3 Make transfer
-    const tokenErcStandard = tokenType;
+    // # 5 Make transfer
     const txReceipts = await userSender.makeTransfer({
-      tokenContractAddress,
-      tokenErcStandard,
-      value,
-      tokenId,
+      tokenContractAddress: config.tokenContractAddress,
+      value: config.value,
+      // tokenId: config.tokenId,
       recipientNightfallAddress: userRecipient.getNightfallAddress(),
       // isOffChain: true,
     });
     console.log("Transaction receipts", txReceipts);
 
-    // # 4 [OPTIONAL] You can check the transaction hash
+    // # 6 [OPTIONAL] You can check the transaction hash
     console.log(
       "Nightfall deposit tx hashes",
       userSender.nightfallTransferTxHashes,
     );
 
-    // # 5 [OPTIONAL] You can check transfers that are not yet in a block
+    // # 7 [OPTIONAL] You can check transfers that are not yet in a block
     const pendingTransfers = await userSender.checkPendingTransfers();
     console.log("Pending balances", pendingTransfers);
   } catch (error) {
