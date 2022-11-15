17ca93bfb6a887a229827aed30d17c08d157fc30
Stefan Velkoski <110013977+velkoskis@users.noreply.github.com>
2022-11-15 18:26:23 +0100

    chore: update contract addresses and versioning (#148)
    * chore: chore:update contract addresses and versioning
    
    * chore: update contract addresses and versioning

--- examples/web-app/src/components/Constants.js
+++ examples/web-app/src/components/Constants.js
@@ -1,6 +1,6 @@
-const erc20ContractAddress = "0xe721F2D97c58b1D1ccd0C80B88256a152d27f0Fe";
-const erc721ContractAddress = "0x7F68ba0dB1D62fB166758Fe5Ef10853537F8DFc5";
-const erc1155ContractAddress = "0xdA0107986bC43E207D0Bb4D9c9a22d35e09db425";
+const erc20ContractAddress = "0x4315287906f3FCF2345Ad1bfE0f682457b041Fa7";
+const erc721ContractAddress = "0x59eE10Cf73e3cfCDE03c8813f2Ad57eaC2248248";
+const erc1155ContractAddress = "0x7715887dfCC755f81DB74c11afB27aC0675eD613";
 const erc20value = "0.0001";
 
 export {
